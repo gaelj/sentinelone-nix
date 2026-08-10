6657b673d4b0027ca74114758bb8ac92618a945f
Gaël James <gaeljames@gmail.com>
2026-08-10 20:59:24 +0200

    Optionally allow manual service stop

--- module.nix
+++ module.nix
@@ -110,6 +110,7 @@ in
           description = "Directory in which the agent stores its runtime data.";
           default = "/var/lib/sentinelone";
         };
+        allowManualServiceStop = mkEnableOption "Allow manual stopping of the service. Keep disabled for security ; enable for debugging purposes";
       };
     };
   };
@@ -244,7 +245,7 @@ in
           "uptrack-prefetch.service"
           "uptrack.service"
         ];
-        RefuseManualStop = "yes";
+        RefuseManualStop = if cfg.allowManualServiceStop then "no" else "yes";
         StartLimitInterval = "90";
         StartLimitBurst = "4";
         RequiresMountsFor = [
